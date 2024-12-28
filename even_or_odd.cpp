694bef143b004fd9533276725aec00e6542f61b7
Ansu-02 <angshupyythoon@gmail.com>
2024-12-29 00:15:20 +0530

    new file to check a number, whether it is even or odd

---
+++ even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+    if(n%2==0){
+        std::cout << "Even";
+    }
+    else{
+        std::cout << "Odd";
+    }
+    return 0;
+}
