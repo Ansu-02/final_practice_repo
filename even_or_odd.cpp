a76227980baf3f9b332536196cbed70cec884a9a
Ansu-02 <angshupyythoon@gmail.com>
2024-12-29 00:27:55 +0530

    new feature is added which is nothing bu chekcing a number is prime or not

--- even_or_odd.cpp
+++ even_or_odd.cpp
@@ -9,9 +9,29 @@ void even_or_odd(int n){
     }
 }
 
+void is_prime(int n){
+    int flag = 0;
+    for(int i=2; i<=sqrt(n); i++){
+        if(n%i==0){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0){
+        std::cout << "Prime";
+    }
+    else{
+        std::cout << "Not Prime";
+    }
+}
+
 int main(){
     int n;
     std::cin >> n;
+    std::cout << "Prime or Not Prime: ";
+    is_prime(n);
+    std::cout << "\nEven or Odd: ";
     even_or_odd(n);
+
     return 0;
 }
